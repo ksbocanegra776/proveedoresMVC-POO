91fbbeba58eaf778864682ea00d108cb1d3852c0
ksbocanegra776 <ksbocanegra776@misena.edu.co>
2022-03-27 12:12:36 -0500

    cambio conexion a bd

--- Model/conexionDB.php
+++ Model/conexionDB.php
@@ -4,7 +4,7 @@ class ConexionDB {
 
     public static function conexion(){
 
-            $conn= new PDO('mysql:host=localhost;dbname=proveedores;charset=utf8','root','');
+            $conn= new PDO('mysql:bdvrptxv379z0mfxlqfj-mysql.services.clever-cloud.com;dbname=bdvrptxv379z0mfxlqfj;charset=utf8','uho8m4rm5iszpxmf','Z3RlOP4mnPM7fdKltLZO');
             $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
             return $conn;
            
